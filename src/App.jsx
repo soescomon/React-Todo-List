0a263966f4627b8d5f80827dfdd5158d1562a72f
Sofia Escobar Monsalve <sescobarmonsalve@gmail.com>
2025-10-01 17:06:21 -0400

    React to do list

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ToDoList from './ToDoList'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <ToDoList/>
+    </>
+  )
+};
+
+export default App
