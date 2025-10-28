05713979ec6ca5cc460ad512a230e162e00bc592
Sofia Escobar Monsalve <sescobarmonsalve@gmail.com>
2025-10-28 11:22:35 -0400

    Update ToDoList using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
 import './App.css'
 import ToDoList from './ToDoList'
-
+const ToDoList1 = [
+  {
+      id: 1,
+      task: "Buy groceries",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      task: "Finish React homework",
+      isCompleted: true,
+    },
+    {
+      id: 3,
+      task: "Walk the dog",
+      isCompleted: false,
+  },
+];
 function App() {
-  const [count, setCount] = useState(0)
-  return (
+   return (
     <>
-      <ToDoList/>
+      <ToDoList tasksList={ToDoList1}/>
     </>
   )
 };
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,34 +1,17 @@
-function ToDoList() {
-      const ToDoList1 = [
-    {
-      id: 1,
-      task: "Buy groceries",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      task: "Finish React homework",
-      isCompleted: true,
-    },
-    {
-      id: 3,
-      task: "Walk the dog",
-      isCompleted: false,
-    },
-  ];
+function ToDoList({tasksList}) {   
   return (
     <>
         <h1>My To Do List</h1>
         <h2>Using index</h2>
         <ol>
-            <li>{ToDoList1[0].task} - {ToDoList1[0].isCompleted ? "Done" : "To Do!"}</li>
-            <li>{ToDoList1[1].task} - {ToDoList1[1].isCompleted ? "Done" : "To Do!"}</li>
-            <li>{ToDoList1[2].task} - {ToDoList1[2].isCompleted ? "Done" : "To Do!"}</li>
+            <li>{tasksList[0].task} - {tasksList[0].isCompleted ? "Done" : "To Do!"}</li>
+            <li>{tasksList[1].task} - {tasksList[1].isCompleted ? "Done" : "To Do!"}</li>
+            <li>{tasksList[2].task} - {tasksList[2].isCompleted ? "Done" : "To Do!"}</li>
         </ol>
 
         <h2>Using .map()</h2>
         <ol>
-            {ToDoList1.map((item) => (
+            {tasksList.map((item) => (
                 <li key={item.id}>{item.task} - {item.isCompleted ? "Done" : "To Do!"}</li>
             ))}
         </ol>
